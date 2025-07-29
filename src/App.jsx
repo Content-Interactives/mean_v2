153830593331324947f87171df023d8350e466de
JoelCCK12 <joel.cruz.intern@ck12.org>
2025-07-29 09:39:49 -0700

    interactive base

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MeanV2 from './components/MeanV2'
+
+
+function App() {
+  return (
+      <div className="App">
+        <MeanV2 />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/MeanV2.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const MeanV2 = () => {
+
+
+	return (
+        <div></div>
+)
+};
+
+
+export default MeanV2;
