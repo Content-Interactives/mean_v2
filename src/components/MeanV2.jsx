3580ee612d093fa9fb13ec2eff2686a5f7509212
JoelCCK12 <joel.cruz.intern@ck12.org>
2025-07-29 10:12:44 -0700

    added reusable components and new base

--- src/components/MeanV2.jsx
+++ src/components/MeanV2.jsx
@@ -1,13 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
-
+import React from 'react';
+import { Container } from './ui/reused-ui/Container';
 
 const MeanV2 = () => {
-
-
 	return (
-        <div>test</div>
-)
-};
-
+		<Container 
+			text="MeanV2" 
+			titleColor="#008545"
+			innerBorderColor="#B3DAC7"
+			innerBackgroundColor="#E8EDF5"
+		>
+			<div>
+				
+			</div>
+		</Container>
+	)
+}
 
 export default MeanV2;
